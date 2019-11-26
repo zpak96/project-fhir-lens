c3dacef03278a58be8eb551bd17662d8954696e1
zpak96 <zpaksi@olivetcollege.edu>
2019-11-25 19:45:17 -0500

    restructuring for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='project-fhir-lens',
+    version='1.0dev',
+    packages=['fhirlens'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
