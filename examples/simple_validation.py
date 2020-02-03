5a640ed5631b1be31b6953bbad22de9cebab5ef0
zpaksi <zane.paksi@mihin.org>
2020-02-03 16:52:29 -0500

    updating test examples

--- examples/simple_validation.py
+++ examples/simple_validation.py
@@ -6,18 +6,10 @@ from datetime import datetime
 # Here's a quick example of utilizing the validator and outputting the result to a txt file
 
 def main():
-    # Instantiate a validator -> fhirlens.Validator() defaults to latest FHIR version: R4
+
     validator = fhirlens.Validator()
-    # Other options include:
-    ##### fhirlens.R4() -> Specifically utilizes R4 schema
-    ##### fhirlens.STU3() -> Specifically utilizes STU3 schema
 
-    # Choose a method of validation. Methods will return output as dict -> {file/resource_name : Validation_result}
-    #### boolValidate: returns dict with boolean values
-    #### depthValidate: returns dict with exact errors in resource -> {file/resource_name: {error_message: schema_location}}
-
-    # output = validator.depthValidate(folder="validate/")
-    output = validator.boolValidate(folder="validate/")
+    output = validator.validate(folder="C:\\Users\\Zane Paksi\\Desktop\\validate")
     
     filename = "Output" + datetime.now().strftime("%m%d-%H%M-%S") + ".txt"
 
