65cb92ff49659235a09894a95f7393847346ceb3
ZanePaksi <zane.paksi@outlook.com>
2023-07-02 16:01:33 -0400

    Expanded test

--- test/test_rito.py
+++ test/test_rito.py
@@ -1,18 +1,31 @@
 from pathlib import Path
 from rito import rito
+import logging
 import pytest
 import os
 
-TEST_PARAMS = [rito.Validator.r5(), rito.Validator.r4(), rito.Validator.stu3(),]
+
+LOGGER = logging.getLogger(__name__)
+TEST_VALIDATORS = [rito.Validator.r5(), rito.Validator.r4(), rito.Validator.stu3()]
+TEST_RESOURCES = ['patient', 'organization', 'observation']
 RESOURCE_PATH = os.path.join(os.path.dirname(__file__), Path('resources'))
 
 
-@pytest.fixture(name='validator', scope='module', params=TEST_PARAMS)
+@pytest.fixture(name='resource', scope='module', params=TEST_RESOURCES)
+def fixture_resource(request):
+    return request.param
+
+
+@pytest.fixture(name='validator', scope='module', params=TEST_VALIDATORS)
 def fixture_validator(request):
     return request.param
 
 
-def test_single_patient_valid(validator):
-    resource_name = f'{validator.get_fhir_version()}_patient.json'
+def test_single_resource_valid(validator, resource):
+    fhir_version = validator.get_fhir_version()
+    resource_name = f'{fhir_version}_{resource}.json'
+    LOGGER.info(f'Validator Version :: {fhir_version}')
+    LOGGER.info(f'Resource File :: {resource_name}')
+
     result = validator.fhir_validate(RESOURCE_PATH + f'\\{resource_name}')
     assert result[resource_name] is True
