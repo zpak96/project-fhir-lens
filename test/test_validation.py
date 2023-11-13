efb1ea9f6e3333ffb8f0147586b27c210b5f0137
ZanePaksi <zane.paksi@outlook.com>
2023-11-12 20:53:38 -0500

    added valid verbose testing for file and dir methods

--- test/test_validation.py
+++ test/test_validation.py
@@ -8,7 +8,7 @@ import os
 LOGGER = logging.getLogger(__name__)
 TEST_VALIDATORS = [rito.Validator.r5(), rito.Validator.r4(), rito.Validator.stu3()]
 TEST_RESOURCES = ['patient', 'organization', 'observation']
-DIRECTORY_PATH = os.path.join(Path(__file__).resolve().parent, Path('data'))
+TEST_DATA_BASE_PATH = os.path.join(Path(__file__).resolve().parent, Path('data'))
 
 
 @pytest.fixture(name='resource', scope='module', params=TEST_RESOURCES)
@@ -24,10 +24,26 @@ def fixture_validator(request):
 def test_valid_fast_file_validate(validator, resource):
     fhir_version = validator.fhir_version
     resource_name = f'{resource}.json'
+
     LOGGER.info(f'Validator Version :: {fhir_version}')
     LOGGER.info(f'Resource File :: {resource}')
 
-    result = validator.file_validate(DIRECTORY_PATH + f'/{fhir_version}/{resource}.json')
+    file_path = TEST_DATA_BASE_PATH + f'/{fhir_version}/{resource}.json'
+    result = validator.file_validate(file_path)
+
+    assert result[resource_name] is True
+
+
+def test_valid_verbose_file_validate(validator, resource):
+    fhir_version = validator.fhir_version
+    resource_name = f'{resource}.json'
+
+    LOGGER.info(f'Validator Version :: {fhir_version}')
+    LOGGER.info(f'Resource File :: {resource}')
+
+    file_path = TEST_DATA_BASE_PATH + f'/{fhir_version}/{resource}.json'
+    result = validator.file_validate(file_path, verbose=True)
+
     assert result[resource_name] is True
 
 
@@ -35,5 +51,17 @@ def test_valid_fast_dir_validate(validator):
     fhir_version = validator.fhir_version
     LOGGER.info(f'Validator Version :: {fhir_version}')
 
-    result = validator.dir_validate(DIRECTORY_PATH + f'/{fhir_version}')
+    directory_path = TEST_DATA_BASE_PATH + f'/{fhir_version}'
+    result = validator.dir_validate(directory_path)
+
+    assert result == {'observation.json': True, 'organization.json': True, 'patient.json': True}
+
+
+def test_valid_verbose_dir_validate(validator):
+    fhir_version = validator.fhir_version
+    LOGGER.info(f'Validator Version :: {fhir_version}')
+
+    directory_path = TEST_DATA_BASE_PATH + f'/{fhir_version}'
+    result = validator.dir_validate(directory_path, verbose=True)
+
     assert result == {'observation.json': True, 'organization.json': True, 'patient.json': True}
